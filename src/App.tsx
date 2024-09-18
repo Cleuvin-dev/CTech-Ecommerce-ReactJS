31ae151e4fcccb6b1b99922662a7ad2b85b0cbeb
Cleuvin-Dev <cleuvin.dev@gmail.com>
2024-09-17 22:02:10 -0300

    React first render test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
 import "./App.css";
+import NotebookImg from "./assets/Notebook.png";
+import cartImg from "./assets/cart.svg.svg";
 
 function App() {
-  return <h1>CTech Ecommerce</h1>;
+  return (
+    <>
+      <header className="ctech-header-client">
+        <nav className="ctech-container">
+          <h1>C-Tech ECommerce</h1>
+          <div className="ctech-navbar-rigth">
+            <div className="ctech-menu-items-container">
+              <div className="ctech-menu-item">
+                <img src={cartImg} alt="Carrinho de compra" />
+              </div>
+            </div>
+            <a href="./confirmation.html">Entrar</a>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section id="product-details-section" className="ctech-container">
+          <div className="ctech-card ctech-mb20">
+            <div className="ctech-product-details-top ctech-line-bottom">
+              <img src={NotebookImg} alt="Notebook" />
+            </div>
+            <div className="ctech-product-details-bottom">
+              <h3>R$ 5000,00</h3>
+              <h4>Computador Gamer XT Pro</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque
+                culpa consequuntur mollitia quia praesentium maiores molestias
+                alias autem unde dolores voluptatibus nisi eum optio, explicabo
+                reiciendis? Nobis ipsum blanditiis eveniet?
+              </p>
+
+              <div className="ctech-category-container">
+                <div className="ctech-category">Eletronicos</div>
+                <div className="ctech-category">Computadores</div>
+              </div>
+            </div>
+          </div>
+          /* Botões de compra e retorno ao inicio da pagina */
+          <div className="ctech-btn-page-container">
+            <div className="ctech-btn ctech-btn-blue ctech-mb20">Comprar</div>
+            <div className="ctech-btn ctech-btn-white">Inicio</div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
